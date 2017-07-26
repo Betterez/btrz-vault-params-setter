d7dbab1355ef996549a4342a3f18b9f9095374b5
Tal Riginiano <tal@talreg.com>
2017-07-26 08:06:26 -0400

    added users feeder

--- src/runner/runner.go
+++ src/runner/runner.go
@@ -6,12 +6,25 @@ import (
 	"log"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
 )
 
 const (
 	versionNumber = 1
 )
+
+func getAwsUsernames(awsSession *session.Session, iamService *iam.IAM) ([]string, error) {
+	usernames := make([]string, 20)
+	usersResults, err := iamService.ListUsers(&iam.ListUsersInput{})
+	if err != nil {
+		return nil, err
+	}
+	for _, userInformation := range usersResults.Users {
+		usernames = append(usernames, *userInformation.UserName)
+	}
+	return usernames, nil
+}
 
 func main() {
 	awsSession, err := btrzaws.GetAWSSession()
@@ -28,31 +41,29 @@ func main() {
 	//keysMetaData := make([]*iam.AccessKeyMetadata, 40)
 	fmt.Printf("Version %d\n", versionNumber)
 	accessKeysFilter := &iam.ListAccessKeysInput{}
-	usersFilter := &iam.ListUsersInput{}
-	usersFilter.SetMaxItems(80)
 	accessKeysFilter.SetMaxItems(80)
 	searchMask := "[\\w+=,.@-]+"
 	accessKeysFilter.SetUserName(searchMask)
 	policiesInput := &iam.ListUserPoliciesInput{}
 	usersPoliciesNames := make([]string, 0)
 	//accessKeysFilter.SetUserName("qualys2017")
-	usersResults, err := iamService.ListUsers(usersFilter)
+	usernames, err := getAwsUsernames(awsSession, iamService)
 	if err != nil {
-		fmt.Print("aws error", err)
+		fmt.Print("error ", err, "exiting\n")
 		os.Exit(1)
 	}
-	for _, userInformation := range usersResults.Users {
-		policiesInput.SetUserName(*userInformation.UserName)
+	for _, username := range usernames {
+		policiesInput.SetUserName(username)
 		usersPolicies, _ := iamService.ListUserPolicies(policiesInput)
 		for _, currentPolicy := range usersPolicies.PolicyNames {
 			usersPoliciesNames = append(usersPoliciesNames, *currentPolicy)
 		}
-		attachedPolicies, _ := iamService.ListAttachedUserPolicies(&iam.ListAttachedUserPoliciesInput{UserName: userInformation.UserName})
+		attachedPolicies, _ := iamService.ListAttachedUserPolicies(&iam.ListAttachedUserPoliciesInput{UserName: &username})
 		for _, currentPolicy := range attachedPolicies.AttachedPolicies {
 			usersPoliciesNames = append(usersPoliciesNames, *currentPolicy.PolicyName)
 		}
 		if len(usersPoliciesNames) > 1 {
-			fmt.Printf("Policy listing for %s\n%v\n\n", *userInformation.UserName, usersPoliciesNames)
+			fmt.Printf("Policy listing for %s\n%v\n\n", username, usersPoliciesNames)
 			usersPoliciesNames = make([]string, 0)
 		}
 		// accessKeysFilter.SetUserName(*userInformation.UserName)
