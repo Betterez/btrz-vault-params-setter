1ddb1f351e6f129e852e06cc6de97a8ce75f2e08
Tal Riginiano <tal@talreg.com>
2017-07-29 20:46:17 -0400

    creating roles and attaching policy

--- src/runner/runner.go
+++ src/runner/runner.go
@@ -16,7 +16,7 @@ const (
 )
 
 func getAwsUsernames(awsSession *session.Session, iamService *iam.IAM) ([]string, error) {
-	usernames := make([]string, 20)
+	usernames := make([]string, 0)
 	usersResults, err := iamService.ListUsers(&iam.ListUsersInput{})
 	if err != nil {
 		return nil, err
@@ -27,56 +27,48 @@ func getAwsUsernames(awsSession *session.Session, iamService *iam.IAM) ([]string
 	return usernames, nil
 }
 
-func processUsers(awsSession *session.Session, iamService *iam.IAM) error {
+func getUsersInformation(awsSession *session.Session, iamService *iam.IAM) ([]*btrzaws.AwsUserInfo, error) {
+	usersInfo := make([]*btrzaws.AwsUserInfo, 0)
 	accessKeysFilter := &iam.ListAccessKeysInput{}
 	accessKeysFilter.SetMaxItems(80)
 	searchMask := "[\\w+=,.@-]+"
 	accessKeysFilter.SetUserName(searchMask)
 	policiesInput := &iam.ListUserPoliciesInput{}
-	usersPoliciesNames := make([]string, 0)
 	//accessKeysFilter.SetUserName("qualys2017")
 	usernames, err := getAwsUsernames(awsSession, iamService)
 	if err != nil {
 		fmt.Print("error ", err, "exiting\n")
-		return err
+		return nil, err
 	}
+
 	for _, username := range usernames {
+		currentUser := &btrzaws.AwsUserInfo{Username: username}
+		accessKeys, err := iamService.ListAccessKeys(&iam.ListAccessKeysInput{UserName: &username})
+		if err != nil {
+			return nil, err
+		}
+		for _, accessKey := range accessKeys.AccessKeyMetadata {
+			currentUser.AccessKeys = append(currentUser.AccessKeys, *accessKey.AccessKeyId)
+		}
 		policiesInput.SetUserName(username)
 		usersPolicies, _ := iamService.ListUserPolicies(policiesInput)
 		for _, currentPolicy := range usersPolicies.PolicyNames {
-			usersPoliciesNames = append(usersPoliciesNames, *currentPolicy)
+			currentUser.Policies = append(currentUser.Policies, *currentPolicy)
 		}
 		attachedPolicies, _ := iamService.ListAttachedUserPolicies(&iam.ListAttachedUserPoliciesInput{UserName: &username})
 		for _, currentPolicy := range attachedPolicies.AttachedPolicies {
-			usersPoliciesNames = append(usersPoliciesNames, *currentPolicy.PolicyName)
+			currentUser.Policies = append(currentUser.Policies, *currentPolicy.PolicyName)
 		}
-		if len(usersPoliciesNames) > 1 {
-			fmt.Printf("Policy listing for %s\n%v\n\n", username, usersPoliciesNames)
-			usersPoliciesNames = make([]string, 0)
-		}
+		// if len(currentUser.Policies) > 1 {
+		// 	fmt.Printf("Policy listing for %s\n%v\n\n", username, currentUser.Policies)
+		// }
+		usersInfo = append(usersInfo, currentUser)
 	}
-	return nil
+	return usersInfo, nil
 
 }
 
-func setup() {
-	awsSession, err := btrzaws.GetAWSSession()
-	if err != nil {
-		fmt.Print(err, "can't get a session")
-		os.Exit(1)
-	}
-	log.Println("session created")
-	iamService := iam.New(awsSession)
-	if iamService == nil {
-		fmt.Println("can't create iam")
-		os.Exit(1)
-	}
-	//keysMetaData := make([]*iam.AccessKeyMetadata, 40)
-	fmt.Printf("Version %d\n", versionNumber)
-	processUsers(awsSession, iamService)
-}
-
-func pullUsersKeysFromCSV(filename string) ([]string, error) {
+func getUsersKeysFromCSV(filename string) ([]string, error) {
 	var err error
 	var record []string
 	keysMap := make(map[string]int, 0)
@@ -105,24 +97,104 @@ func pullUsersKeysFromCSV(filename string) ([]string, error) {
 	}
 	return usersKeys, nil
 }
-func main() {
-	keys, err := pullUsersKeysFromCSV("/home/tal/Documents/programming/go/scanner/dump/output2017-07-26 09:15:32.341402017 -0400 EDT.csv")
+
+func crossKeysWithUsers(awsSession *session.Session, iamService *iam.IAM) ([]*btrzaws.AwsUserInfo, error) {
+	usersInfo, err := getUsersInformation(awsSession, iamService)
 	if err != nil {
-		fmt.Printf("Error %v. exiting\n", err)
+		return nil, err
+	}
+	existingKeys, err := getUsersKeysFromCSV("/home/tal/Documents/programming/go/scanner/dump/output2017-07-26 09:15:32.341402017 -0400 EDT.csv")
+	if err != nil {
+		return nil, err
+	}
+	keysMap := make(map[string]*btrzaws.AwsUserInfo)
+	for _, user := range usersInfo {
+		for _, currentUserKey := range user.AccessKeys {
+			keysMap[currentUserKey] = user
+		}
+	}
+	foundUsers := make([]*btrzaws.AwsUserInfo, 0)
+	for _, key := range existingKeys {
+		if _, exists := keysMap[key]; exists {
+			foundUsers = append(foundUsers, keysMap[key])
+		}
+	}
+	return foundUsers, nil
+}
+
+func setup() {
+	awsSession, err := btrzaws.GetAWSSession()
+	if err != nil {
+		fmt.Print(err, "can't get a session")
 		os.Exit(1)
 	}
-	for index, key := range keys {
-		fmt.Printf("%d.\t%s\n", index, key)
+	log.Println("session created")
+	iamService := iam.New(awsSession)
+	if iamService == nil {
+		fmt.Println("can't create iam")
+		os.Exit(1)
 	}
-	// accessKeysFilter.SetUserName(*userInformation.UserName)
-	// keyResults, err := iamService.ListAccessKeys(accessKeysFilter)
-	// if err != nil {
-	// 	fmt.Print("aws error", err)
-	// 	os.Exit(1)
-	// }
-	// for _, keyInfo := range keyResults.AccessKeyMetadata {
-	// 	keysMetaData = append(keysMetaData, keyInfo)
-	// }
+	username := "lartoTest"
+	fmt.Println("creating user")
+	output, err := iamService.CreateUser(&iam.CreateUserInput{UserName: &username})
+	if err != nil {
+		fmt.Println(err, "exiting")
+		os.Exit(1)
+	}
+	fmt.Println("created at ", *output.User.CreateDate)
+
+	iamService.DeleteUser(&iam.DeleteUserInput{UserName: &username})
+	//keysMetaData := make([]*iam.AccessKeyMetadata, 40)
+	/*fmt.Printf("Version %d\n", versionNumber)
+	usersInfo, err := crossKeysWithUsers(awsSession, iamService)
+	if err != nil {
+		fmt.Println(err, "terminating")
+		os.Exit(1)
+	}
+	for _, item := range usersInfo {
+		fmt.Println(item.ToString())
+	}*/
+}
+
+func createTestRole() {
+	awsSession, err := btrzaws.GetAWSSession()
+	if err != nil {
+		fmt.Print(err, "can't get a session")
+		os.Exit(1)
+	}
+	log.Println("session created")
+	iamService := iam.New(awsSession)
+	if iamService == nil {
+		fmt.Println("can't create iam")
+		os.Exit(1)
+	}
+	policyDocument := `{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"Service":["s3.amazonaws.com"]},"Action":["sts:AssumeRole"]}]}`
+	roleName := "TheTestRole2"
+	policyName := "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+	path := "/"
+	resp, err := iamService.CreateRole(&iam.CreateRoleInput{
+		AssumeRolePolicyDocument: &policyDocument,
+		RoleName:                 &roleName,
+		Path:                     &path,
+	})
+	if err != nil {
+		fmt.Print(err, "exiting", "\n")
+		os.Exit(1)
+	}
+	fmt.Println(resp.String(), "created.")
+	policyResponse, err := iamService.AttachRolePolicy(&iam.AttachRolePolicyInput{
+		RoleName:  &roleName,
+		PolicyArn: &policyName,
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(policyResponse)
+}
+
+func main() {
+	createTestRole()
 }
 
 //fmt.Print(keysMetaData)
