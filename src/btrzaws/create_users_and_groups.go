61ffbd4c36d46e1be4808cd357e2e8a584fda910
Tal Riginiano <tal@talreg.com>
2017-08-14 07:35:05 -0400

    refactoring

--- src/btrzaws/create_users_and_groups.go
+++ src/btrzaws/create_users_and_groups.go
@@ -10,64 +10,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/iam"
 )
 
-// MongoInformation - service mongo information
-type MongoInformation struct {
-	DatabaseRole string              `json:"role"`
-	DatabaseName []map[string]string `json:"database_name"`
-}
-
-// ServiceInformation - service informaito needed to create groups and users
-type ServiceInformation struct {
-	ServiceName          string           `json:"service_name"`
-	RequiredEnvironments []string         `json:"environments"`
-	RequiredArn          []string         `json:"arns"`
-	Path                 string           `json:"path"`
-	MongoSettings        MongoInformation `json:"mongodb"`
-}
-
-// GenerateServiceInformation - create a ServiceInformation with default settings
-func GenerateServiceInformation(serviceName string) *ServiceInformation {
-	return &ServiceInformation{
-		ServiceName:          serviceName,
-		RequiredEnvironments: []string{"staging", "sandbox", "production"},
-		RequiredArn:          []string{},
-		Path:                 "/",
-	}
-}
-
-// AddServiceArn - adds an aws arn to the service request
-func (si *ServiceInformation) AddServiceArn(arn string) {
-	si.RequiredArn = append(si.RequiredArn, arn)
-}
-
-// GetVaultPath - return the vault (+secret) path for this service
-func (si *ServiceInformation) GetVaultPath() string {
-	return "secret/" + si.ServiceName
-}
-
-// GetGroupName - return the group name for the service
-func (si *ServiceInformation) GetGroupName() string {
-	return fmt.Sprintf("%s-Group", si.ServiceName)
-}
-
-// IsInformationOK - Checks if the informaito provided is OK to process
-func (si *ServiceInformation) IsInformationOK() bool {
-	if si.ServiceName == "" {
-		return false
-	}
-	if si.RequiredArn == nil || si.RequiredEnvironments == nil {
-		return false
-	}
-
-	if len(si.RequiredEnvironments) == 0 {
-		return false
-	}
-	if len(si.RequiredArn) == 0 {
-		return false
-	}
-	return true
-}
-
 // CreateGroupAndUsersForService - for a given service name creates a groups, users and stores the keys in the vault
 func CreateGroupAndUsersForService(awsSession *session.Session, iamService *iam.IAM, serviceInfo *ServiceInformation) error {
 	if serviceInfo.IsInformationOK() == false {
---
+++ src/btrzaws/service_information.go
@@ -0,0 +1,63 @@
+package btrzaws
+
+import (
+	"fmt"
+)
+
+// MongoInformation - service mongo information
+type MongoInformation struct {
+	DatabaseRole string              `json:"role"`
+	DatabaseName []map[string]string `json:"database_name"`
+}
+
+// ServiceInformation - service informaito needed to create groups and users
+type ServiceInformation struct {
+	ServiceName          string           `json:"service_name"`
+	RequiredEnvironments []string         `json:"environments"`
+	RequiredArn          []string         `json:"arns"`
+	Path                 string           `json:"path"`
+	MongoSettings        MongoInformation `json:"mongodb"`
+}
+
+// GenerateServiceInformation - create a ServiceInformation with default settings
+func GenerateServiceInformation(serviceName string) *ServiceInformation {
+	return &ServiceInformation{
+		ServiceName:          serviceName,
+		RequiredEnvironments: []string{"staging", "sandbox", "production"},
+		RequiredArn:          []string{},
+		Path:                 "/",
+	}
+}
+
+// AddServiceArn - adds an aws arn to the service request
+func (si *ServiceInformation) AddServiceArn(arn string) {
+	si.RequiredArn = append(si.RequiredArn, arn)
+}
+
+// GetVaultPath - return the vault (+secret) path for this service
+func (si *ServiceInformation) GetVaultPath() string {
+	return "secret/" + si.ServiceName
+}
+
+// GetGroupName - return the group name for the service
+func (si *ServiceInformation) GetGroupName() string {
+	return fmt.Sprintf("%s-Group", si.ServiceName)
+}
+
+// IsInformationOK - Checks if the informaito provided is OK to process
+func (si *ServiceInformation) IsInformationOK() bool {
+	if si.ServiceName == "" {
+		return false
+	}
+	if si.RequiredArn == nil || si.RequiredEnvironments == nil {
+		return false
+	}
+
+	if len(si.RequiredEnvironments) == 0 {
+		return false
+	}
+	if len(si.RequiredArn) == 0 {
+		return false
+	}
+	return true
+}
