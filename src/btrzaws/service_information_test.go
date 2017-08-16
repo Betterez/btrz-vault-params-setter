5a4a557c92bdbfaef94517db1beb5f2a45ad143c
Tal Riginiano <tal@talreg.com>
2017-08-16 08:41:27 -0400

    update

--- src/btrzaws/service_information_test.go
+++ src/btrzaws/service_information_test.go
@@ -1,20 +1,21 @@
 package btrzaws
-import(
-  "testing"
-  "os"
-  "encoding/json"
-  "io/ioutil"
-)
-const(
-  ServicesFile="../../sample_files/services.json"
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"testing"
 )
 
-func TestServiceAPIKeys(t*testing.T)  {
-  fileData,err:=ioutil.ReadFile(ServicesFile)
-  if err!=nil{
-    t.Fatal(err)
-  }
-  info:=&ServiceInformation{}
-  json.Unmarshal(fileData, info)
+const (
+	ServicesFile = "../../sample_files/services.json"
+)
+
+func TestServiceAPIKeys(t *testing.T) {
+	fileData, err := ioutil.ReadFile(ServicesFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	info := &ServiceInformation{}
+	json.Unmarshal(fileData, info)
 
 }
